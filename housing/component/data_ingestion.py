ae80d1d6ac3e338af14066ae0ccdf737762cb2fb
vini2309 <86472870+vini2309@users.noreply.github.com>
2023-01-27 19:52:41 +0530

    Updated componet-data-Ingestion.py file

---
+++ housing/component/data_ingestion.py
@@ -0,0 +1,20 @@
+import sys, os
+from housing.config import DataIngestionConfig
+from housing.exception import HousingException
+from housing.logger import logging
+
+
+class DataIngestion:
+
+    def __init__(self, data_ingestion_config: DataIngestionConfig):
+        try:
+            logging.info(f"{'='*20} Data Ingestion log started.{'='*20}")
+            self.data_ingestion_config = data_ingestion_config
+        except Exception as e:
+            raise HousingException(e, sys) from e
+
+    def initiate_data_ingestion(self)-> DataIngestionArtifact:
+        try:
+            pass
+        except Exception as e:
+            raise HousingException(e, sys) from e
