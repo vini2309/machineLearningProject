7d20cffec0f6436209c2ee01e7eebeb25392f7e8
vini2309 <86472870+vini2309@users.noreply.github.com>
2023-01-28 09:39:46 +0530

    updated logging file

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -18,7 +18,7 @@ class HousingException(Exception):
         """
         _,_,exec_tb = error_detail.exc_info()
 
-        line_number = exec_tb.tb_frame.f_lineno
+        line_number = exec_tb.tb_lineno
         file_name = exec_tb.tb_frame.f_code.co_filename
 
         error_message = f"Error occured in script: [{file_name}] at line number: [{line_number}] error_message: [{error_message}]"
