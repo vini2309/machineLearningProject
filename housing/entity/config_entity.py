ded5f88d219c29ad9cd8d523ebec292a02e40c47
vini2309 <86472870+vini2309@users.noreply.github.com>
2023-02-02 11:17:53 +0530

    Added data_transformation

--- housing/entity/config_entity.py
+++ housing/entity/config_entity.py
@@ -6,7 +6,7 @@ DataIngestionConfig = namedtuple("DataIngestionConfig",
 DataValidationConfig = namedtuple("DataValidationConfig", ["schema_file_path", "report_file_path", "report_page_file_path"])
 
 DataTransformationConfig = namedtuple("DataTransformationConfig", ["add_bedroom_per_room",
-                                                                "tranformed_train_dir",
+                                                                "transformed_train_dir",
                                                                 "transformed_test_dir",
                                                                 "preprocessed_object_file_path"])
 
