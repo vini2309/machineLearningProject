95cf2518b5a7162f6e9d2e0a2fb107d2db9b7722
vini2309 <86472870+vini2309@users.noreply.github.com>
2023-02-16 23:14:56 +0530

    Updated transformation.py file

--- demo.py
+++ demo.py
@@ -1,15 +1,23 @@
 from housing.pipeline.pipeline import Pipeline
 from housing.logger import logging
 from housing.config.configuration import Configuration
+from housing.component.data_transformation import DataTransformation
+from housing.util.util import read_yaml_file
 
 def main():
     try:
 
-        #pipeline = Pipeline()
-        #pipeline.run_pipeline()
-        data_validation_config = Configuration().get_data_transformation_config()
-        print(data_validation_config)
-        
+        pipeline = Pipeline()
+        pipeline.run_pipeline()
+        #data_validation_config = Configuration().get_data_transformation_config()
+        #print(data_validation_config)
+        #schema_file_path = r"C:\Users\Praveen\machineLearningProject\config\schema.yaml"
+        #file_path = r"C:\Users\Praveen\machineLearningProject\housing\artifact\data_ingestion\2023-01-30-09-27-22\ingested_data\train\housing.csv"
+        #df = DataTransformation.load_data(file_path=file_path, schema_file_path=schema_file_path)
+        #print(read_yaml_file(file_path=schema_file_path))
+        #print(df.columns)
+        #print(df.dtypes)
+
     except Exception as e:
         logging.error(f"{e}")
         print(e)
--- housing/util/util.py
+++ housing/util/util.py
@@ -1,9 +1,13 @@
 import yaml
 from housing.exception import HousingException
 import os,sys
+import numpy as np
+import dill
+import pandas as pd
+from housing.constant import *
 
 
-def read_yaml_file(file_path:str) -> dict:
+def read_yaml_file(file_path) -> dict:
     """
     Reads a YAML file and returns the contents as a dictionary.
     file_path : str
@@ -14,3 +18,80 @@ def read_yaml_file(file_path:str) -> dict:
             return yaml.safe_load(yaml_file)
     except Exception as e:
         raise HousingException(e,sys) from e
+
+
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise HousingException(e, sys) from e
+
+
+def load_numpy_array_data(file_path: str) -> np.array:
+    """
+    load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded
+    """
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise HousingException(e, sys) from e
+
+def save_object(file_path:str,obj):
+    """
+    file_path: str
+    obj: Any sort of object
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+    except Exception as e:
+        raise HousingException(e,sys) from e
+
+
+def load_object(file_path:str):
+    """
+    file_path: str
+    """
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise HousingException(e,sys) from e
+
+##As we are not consuming anything from the self method we can declare this function as static method
+def load_data(file_path:str, schema_file_path:str)-> pd.DataFrame: # Checking the schema data columns is there in actual dataframe
+    try:
+        dataset_schema = read_yaml_file(schema_file_path)  # The schema file data available as a dictionary
+
+        schema = dataset_schema[DATASET_SCHEMA_COLUMN_KEYS]
+
+        dataframe = pd.read_csv(file_path)
+
+        error_message = ""
+
+        for column in dataframe.columns:
+            if column in list(schema.keys()):
+                dataframe[column].astype(schema[column])
+            else:
+                error_message = f"{error_message} \ncolumn: [{column}] is not in schema. "
+            
+        if len(error_message) > 0:
+            raise Exception(error_message)
+
+        return dataframe
+
+    except Exception as e:
+        raise HousingException(e, sys) from e
